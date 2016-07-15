db0e1e3ed066b9d7106dbd7bb0730d0d9456ce85
Andrew <andrew.j.heinke@gmail.com>
2016-07-14 20:30:29 -0500

    customer view complete

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,26 +1,56 @@
-// First Display All of the Items available for sale. This initial display, should include the ids, names, and prices of products for sale
-//
-// Users should then be prompted with two messages. The first message should ask them the ID of the product they would like to buy. The second message should ask them how many of the product they would like to buy.
-
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "root", //Your username
-    password: "", //Your password
+    user: "root",
+    password: "",
     database: "Bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-});
+var printTable = function() {
+  connection.query('SELECT * FROM Products', function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+          console.log(res[i].ItemID + " | " + res[i].ProductName + " | " + res[i].DepartmentName + " | " + res[i].Price + " | " + res[i].StockQuantity);
+      }
+      console.log("-----------------------------------");
+  });
+};
+printTable();
 
-connection.query('SELECT * FROM Products', function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-        console.log(res[i].ItemID + " | " + res[i].ProductName + " | " + res[i].DepartmentName + " | " + res[i].Price + " | " + res[i].StockQuantity);
+inquirer.prompt([
+{
+	type: "input",
+	message: "Welcome to Bamazon! What is the ID of the item you would like to purchase?",
+	name: "idSelect"
+},
+{
+	type:"input",
+	message: "How many would you like to purchase?",
+	name: "quantity"
+}
+]).then(function(answers){
+  chosenItem = answers.idSelect;
+  itemQuantity = answers.quantity;
+
+  connection.query('SELECT ItemID, ProductName, DepartmentName, Price, StockQuantity FROM Products WHERE ItemID= ' + chosenItem,
+		function(err, res) {
+    if(err) throw err;
+    if(res[0].StockQuantity < itemQuantity){
+			console.log("Insufficient quantity, there isn\'t enough of this item in stock. Please select an amount that is less than or equal to " + res[0].StockQuantity);
+		}
+    else {
+			connection.query("UPDATE Products SET? WHERE?",
+				[{StockQuantity: res[0].StockQuantity - itemQuantity}, {ItemID: chosenItem}],
+				function(err, result){});
+    if (itemQuantity === '1') {
+      console.log("Total Cost: $" + (res[0].Price * itemQuantity) + " for buying " + itemQuantity + " " + res[0].ProductName);
+    } else {
+        console.log("Total Cost: $" + (res[0].Price * itemQuantity) + " for buying " + itemQuantity + " " + res[0].ProductName + "s");
     }
-    console.log("-----------------------------------");
+		console.log("Store database has been updated");
+		printTable();
+		}
+  });
 });
